0950f8d27d1c2f8da6e14d475e64e6248d6e75c1
J Boyer <jason@fpcomplete.com>
2017-06-18 17:31:36 -0400

    vpc-scenario-1: update to use the refactored vpc and route-public modules

--- tf-modules/vpc-scenario-1/main.tf
+++ tf-modules/vpc-scenario-1/main.tf
@@ -9,56 +9,31 @@
  * http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Scenarios.html
  *
  */
-resource "aws_vpc" "main" {
-  cidr_block           = "${var.cidr}"
+
+module "vpc" {
+  source               = "../vpc"
+  region               = "${var.region}"
+  cidr                 = "${var.cidr}"
+  name_prefix          = "${var.name_prefix}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
   enable_dns_support   = "${var.enable_dns_support}"
-  tags = "${merge(map("Name", "${var.name_prefix}"), "${var.extra_tags}")}"
-}
-
-// move these two into their own module?
-resource "aws_vpc_dhcp_options" "main" {
-  domain_name         = "${var.region}.compute.internal"
-  domain_name_servers = "${var.dns_servers}"
-
-  tags = "${merge(map("Name", "${var.name_prefix}"), "${var.extra_tags}")}"
-}
-
-resource "aws_vpc_dhcp_options_association" "main" {
-  vpc_id          = "${aws_vpc.main.id}"
-  dhcp_options_id = "${aws_vpc_dhcp_options.main.id}"
+  dns_servers          = ["${var.dns_servers}"]
+  extra_tags           = "${var.extra_tags}"
 }
 
 module "public-subnets" {
   source      = "../subnets"
   azs         = "${var.azs}"
-  vpc_id      = "${aws_vpc.main.id}"
+  vpc_id      = "${module.vpc.id}"
   name_prefix = "${var.name_prefix}-public"
   cidr_blocks = "${var.public_subnet_cidrs}"
   extra_tags  = "${var.extra_tags}"
 }
 
-// Internet Gateway - provide internet access to public subnets.
-resource "aws_internet_gateway" "main" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  tags = "${merge(map("Name", "${var.name_prefix}"), "${var.extra_tags}")}"
-}
-
-// route table and link to subnets
-resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.main.id}"
-  }
-
-  tags = "${merge(map("Name", "${var.name_prefix}-public"), "${var.extra_tags}")}"
-}
-
-resource "aws_route_table_association" "public-rta" {
-  count          = "${length(var.public_subnet_cidrs)}"
-  subnet_id      = "${element(module.public-subnets.ids, count.index)}"
-  route_table_id = "${aws_route_table.public.id}"
+module "public-gateway" {
+  source      = "../route-public"
+  vpc_id      = "${module.vpc.id}"
+  name_prefix = "${var.name_prefix}-public"
+  extra_tags  = "${var.extra_tags}"
+  public_subnet_ids = ["${module.public-subnets.ids}"]
 }
