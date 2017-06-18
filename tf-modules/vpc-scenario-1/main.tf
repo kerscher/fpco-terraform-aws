77df5c099a65f8f1767d1724c55a879195388147
J Boyer <jason@fpcomplete.com>
2017-06-18 15:15:40 -0400

    vpc-scenario-1: update to use the refactored subnets module

--- tf-modules/vpc-scenario-1/main.tf
+++ tf-modules/vpc-scenario-1/main.tf
@@ -29,13 +29,13 @@ resource "aws_vpc_dhcp_options_association" "main" {
   dhcp_options_id = "${aws_vpc_dhcp_options.main.id}"
 }
 
-// public subnets
-resource "aws_subnet" "public" {
-  count             = "${length(var.public_subnet_cidrs)}"
-  vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "${var.public_subnet_cidrs[count.index]}"
-  availability_zone = "${element(var.azs, count.index)}"
-  tags              = "${merge(map("Name", "${var.name_prefix}-public-${format("%02d", count.index)}-${element(var.azs, count.index)}"), "${var.extra_tags}")}"
+module "public-subnets" {
+  source      = "../subnets"
+  azs         = "${var.azs}"
+  vpc_id      = "${aws_vpc.main.id}"
+  name_prefix = "${var.name_prefix}-public"
+  cidr_blocks = "${var.public_subnet_cidrs}"
+  extra_tags  = "${var.extra_tags}"
 }
 
 // Internet Gateway - provide internet access to public subnets.
@@ -59,6 +59,6 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public-rta" {
   count          = "${length(var.public_subnet_cidrs)}"
-  subnet_id      = "${element(module.subnets.public_ids, count.index)}"
+  subnet_id      = "${element(module.public-subnets.ids, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
 }
