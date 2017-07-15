e8f7520713535c48d7a8b9d9a2ad33ed6d943402
J Boyer <jason@fpcomplete.com>
2017-07-14 23:12:28 -0400

    persistent-ebs: swap account_arn for aws_caller_identity data source

--- tf-modules/persistent-ebs/variables.tf
+++ tf-modules/persistent-ebs/variables.tf
@@ -32,6 +32,4 @@ variable "iops" {
     default = ""
     description = "The amount of IOPS to provision for the EBS block device"
 }
-variable "account_arn" {
-    description = "The AWS account number, for IAM role assigned to the instance created"
 }
